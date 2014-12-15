3617d7ba955089eb3259c9e67f7a133a62db6a03
gekowa <gekowa@gmail.com>
2014-12-15 23:52:49 +0800

    Use SnappyDB instead

--- app/src/main/java/cn/sdgundam/comicatsdgo/service/TrackingService.java
+++ app/src/main/java/cn/sdgundam/comicatsdgo/service/TrackingService.java
@@ -4,6 +4,12 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.snappydb.DB;
+import com.snappydb.DBFactory;
+import com.snappydb.SnappydbException;
+
+import java.util.Date;
+
 import cn.sdgundam.comicatsdgo.db.TrackingContract;
 import cn.sdgundam.comicatsdgo.db.TrackingDbHelper;
 
@@ -11,20 +17,54 @@ import cn.sdgundam.comicatsdgo.db.TrackingDbHelper;
  * Created by xhguo on 12/15/2014.
  */
 public class TrackingService {
+    static final String TRACKING_TABLE_NAME = "tracking";
+
+    static final String VIDEO_VIEWED_KEY = "VIDEO_VIEWED_%s";
+    static final String UNIT_VIEWED_KEY = "UNIT_VIEWED_%s";
+
     Context context;
 
-    public TrackingService(Context context) {
+    public TrackingService(Context context) throws SnappydbException {
         this.context = context;
     }
 
-    public void TrackObjectView(String objectType, String objectId) {
-        TrackingDbHelper dbHelper = new TrackingDbHelper(context);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+    void markVideoLastViewed(String videoPostId) {
+        try {
+            DB trackingDB = DBFactory.open(context, TRACKING_TABLE_NAME);
+            trackingDB.put(String.format(VIDEO_VIEWED_KEY, videoPostId), new Date());
+            trackingDB.close();
+        } catch (SnappydbException ex) { }
+    }
 
-        ContentValues values = new ContentValues();
-        values.put(TrackingContract.Tracking.COLUMN_NAME_OBJECT_TYPE, objectType);
-        values.put(TrackingContract.Tracking.COLUMN_NAME_OBJECT_ID, objectId);
+    boolean isVideoViewed(String videoPostId) {
+        try {
+            DB trackingDB = DBFactory.open(context, TRACKING_TABLE_NAME);
+            Date d = trackingDB.getObject(String.format(VIDEO_VIEWED_KEY, videoPostId), Date.class);
+            trackingDB.close();
 
-        db.insertWithOnConflict(TrackingContract.Tracking.TABLE_NAME, null, null, SQLiteDatabase.CONFLICT_REPLACE);
+            return d != null;
+        } catch (SnappydbException ex) {
+            return false;
+        }
+    }
+
+    void markUnitViewed(String unitId) {
+        try {
+            DB trackingDB = DBFactory.open(context, TRACKING_TABLE_NAME);
+            trackingDB.put(String.format(UNIT_VIEWED_KEY, unitId), new Date());
+            trackingDB.close();
+        } catch (SnappydbException ex) { }
+    }
+
+    boolean isUnitViewed(String unitId) {
+        try {
+            DB trackingDB = DBFactory.open(context, TRACKING_TABLE_NAME);
+            Date d = trackingDB.getObject(String.format(UNIT_VIEWED_KEY, unitId), Date.class);
+            trackingDB.close();
+
+            return d != null;
+        } catch (SnappydbException ex) {
+            return false;
+        }
     }
 }
